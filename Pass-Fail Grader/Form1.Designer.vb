3efd80114134a15e47f1b23bc0568631c846c1b6
Ahmed Bilal <56721456+ahmedbilal1510@users.noreply.github.com>
2021-03-27 11:48:37 +0900

    Add files via upload

---
+++ Pass-Fail Grader/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(101, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(171, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Test Score"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(115, 263)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 37)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Grade"
+        '
+        'lblResult
+        '
+        Me.lblResult.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblResult.Location = New System.Drawing.Point(502, 253)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(192, 56)
+        Me.lblResult.TabIndex = 2
+        Me.lblResult.Text = "??"
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(509, 66)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(199, 44)
+        Me.txtScore.TabIndex = 3
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(77, 362)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(182, 60)
+        Me.btnRun.TabIndex = 4
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(19.0!, 37.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents txtScore As TextBox
+    Friend WithEvents btnRun As Button
+End Class
---
+++ Pass-Fail Grader/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        Dim score As Integer = CType(txtScore.Text, Integer)
+
+        Select Case score
+            Case 60 To 100
+                lblResult.Text = "Pass"
+                lblResult.BackColor = Color.LightGreen
+            Case Else
+                lblResult.Text = "Fail"
+                lblResult.BackColor = Color.IndianRed
+        End Select
+    End Sub
+End Class
